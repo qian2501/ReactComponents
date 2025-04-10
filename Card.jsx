16d8fb0a90c589e7abeb89de4a86a04f910d126c
qian2501 <qian2501@gmail.com>
2025-04-10 13:26:38 +0800

    update Card style

--- Card.jsx
+++ Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ children, className = '' }) {
     return (
-        <div className={"w-full px-6 py-4 bg-gray-700 shadow-md overflow-hidden sm:rounded-lg " + className}>
+        <div className={"w-full px-6 py-4 border-2 border-gray-700 overflow-hidden sm:rounded-lg " + className}>
             {children}
         </div>
     );
