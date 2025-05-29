162b3b23853a3f6a2bec981c879a8bd3f8a8e55d
qian2501 <qian2501@gmail.com>
2025-05-29 16:00:24 +0800

    optimize parallax

--- ParallaxLayer.jsx
+++ ParallaxLayer.jsx
@@ -2,22 +2,17 @@ import { useRef, useEffect } from 'react';
 
 export default function ParallaxLayer({ className = '', children, offset = 0, scale = 1 }) {
   const layerRef = useRef();
-  const ticking = useRef(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      if (!ticking.current) {
-        window.requestAnimationFrame(() => {
-          const y = window.scrollY;
-          if (layerRef.current) {
-            layerRef.current.style.transform = `translate3d(0, ${offset + y * scale}px, 0)`;
-          }
-          ticking.current = false;
-        });
-        ticking.current = true;
-      }
+      window.requestAnimationFrame(() => {
+        const y = window.scrollY;
+        if (layerRef.current) {
+          layerRef.current.style.transform = `translate3d(0, ${offset + y * scale}px, 0)`;
+        }
+      });
     };
-
+  
     window.addEventListener('scroll', handleScroll, { passive: true });
     return () => window.removeEventListener('scroll', handleScroll);
   }, [offset, scale]);
